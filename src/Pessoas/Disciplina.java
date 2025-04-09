58cd2d084f611fc4df65b9e92c0470b755f72e65
gildi <gilberto.dias@dcx.ufpb.br>
2025-04-09 16:22:19 -0300

    criação classe Disciplina

--- src/Pessoas/Disciplina.java
+++ src/Pessoas/Disciplina.java
@@ -1,27 +1,33 @@
 package Pessoas;
 
 public class Disciplina {
+
     private String nome;
+    private Professor professor;
     private int qtdHoras;
     private int codigo;
     private boolean ativo;
 
-    public Disciplina(String nome, int qtdHoras, int codigo, boolean ativo) {
+    public Disciplina(String nome, int qtdHoras, int codigo) {
         this.nome = nome;
         this.qtdHoras = qtdHoras;
         this.codigo = codigo;
-        this.ativo = ativo;
+        this.ativo = true;
     }
 
-    public String getNome() {
+    public String getNomeDisciplina() {
         return nome;
     }
 
-    public int getQtdHoras() {
+    public int getHorasDisciplina() {
         return qtdHoras;
     }
 
-    public int getCodigo() {
+    public int getCodigoDisciplina() {
         return codigo;
+    }
+
+    public Professor getProfessor() {
+        return professor;
     }
 }
