0e24089bd639e19319416295e3ba62b4687f7cf5
guilherme lopes <guilherme.lopes@dcx.ufpb.br>
2025-04-10 21:01:36 -0300

    Correção da classe Pessoa.java

--- src/entidades/Pessoa.java
+++ src/entidades/Pessoa.java
@@ -1,18 +1,21 @@
 package entidades;
 
 import java.util.Objects;
+import java.util.UUID;
 
 public abstract class Pessoa {
     private final String nome;
     private String telefone;
     private String email;
     private boolean ativo;
+    private UUID codigo;
 
-    public Pessoa(String nome, String telefone, String email, boolean ativo) {
+    public Pessoa(String nome, String telefone, String email, boolean ativo, UUID codigo) {
         this.nome = nome;
         this.telefone = telefone;
         this.email = email;
         this.ativo = ativo;
+        this.codigo = codigo;
     }
 
     public String getNome() {
@@ -30,21 +33,22 @@ public abstract class Pessoa {
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof Pessoa pessoa)) return false;
-        return ativo == pessoa.ativo && Objects.equals(nome, pessoa.nome) && Objects.equals(telefone, pessoa.telefone) && Objects.equals(email, pessoa.email);
+        return ativo == pessoa.ativo && Objects.equals(nome, pessoa.nome) && Objects.equals(telefone, pessoa.telefone) && Objects.equals(email, pessoa.email) && Objects.equals(codigo, pessoa.codigo);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(nome, telefone, email, ativo);
+        return Objects.hash(nome, telefone, email, ativo, codigo);
     }
 
     @Override
     public String toString() {
         return "Pessoa{" +
                 "nome='" + nome + '\'' +
-                ", telefone=" + telefone +
+                ", telefone='" + telefone + '\'' +
                 ", email='" + email + '\'' +
                 ", ativo=" + ativo +
+                ", codigo=" + codigo +
                 '}';
     }
 }
--- src/entidades/Professor.java
+++ src/entidades/Professor.java
@@ -1,7 +1,9 @@
 package entidades;
-public class Professor extends Pessoa {
 
-    public Professor(String nome, String telefone, String email, int codigo) {
-        super(nome, telefone, email, true);
+import java.util.UUID;
+
+public class Professor extends Pessoa {
+    public Professor(String nome, String telefone, String email, boolean ativo, UUID codigo) {
+        super(nome, telefone, email, ativo, codigo);
     }
 }
